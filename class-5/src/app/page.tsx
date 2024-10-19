07165a64f8566850c8d71b4ecdf6cb2f46dcb11c
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2024-10-19 15:31:15 +0500

    class 5 task

--- class-5/src/app/page.tsx
+++ class-5/src/app/page.tsx
@@ -1,15 +1,8 @@
-import Image from "next/image";
-// import Navbar from "./components/Navbar";
-
 export default function Home() {
   return (
-    // <div className="bg-black text-purple-500 font-bold">
-    //   <Navbar />
-    //   <h1>tailwind css</h1>
-    // </div>
+    
     <div className="bg-yellow-600 text-black font-bold">
-      {/* <head> */}
-      {/* <div className="w-full bg-black"> */}
+      
       <nav className="flex h-12 w-full bg-pink-600 justify-between items-center">
         <div className="bg-orange-500 w-56 h-5 text-center">1st Nav div</div>
         <div className="bg-blue-800 w-56 h-5 text-center">2nd Nav div</div>
@@ -22,8 +15,7 @@ export default function Home() {
         <div className="bg-red-600 w-96">This is 3rd div</div>
       </div>
 
-      {/* <p className="text-red-900 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing.</p> */}
+     
     </div>
   );
 }
