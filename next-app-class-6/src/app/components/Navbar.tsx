cc81cf297d72a3a96e3a4d6fff6cbd6d6ea38b1f
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2024-10-30 12:56:08 +0500

    Update Navbar.tsx

--- next-app-class-6/src/app/components/Navbar.tsx
+++ next-app-class-6/src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav>
       <div className="bg-blue-950 pb-2 text-white flex justify-between text-center pt-3">
         <div className="ml-36 flex">
-          <Image src="/phone.png" alt="phone-icon" width={20} height={1} />
+          <Image src="/phone.png" alt="phone-icon" width={20} height={2} />
           <p className="text-xs py-1">Download App via SMS</p>
         </div>
         <div><p className="mr-36 text-xs py-1">Sign In | Sign Up</p></div>
