06d261383ab061e99d41150024e04c766b99c0ec
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2024-10-30 13:31:41 +0500

    Update page.tsx

--- next-app-class-6/src/app/confirmation/page.tsx
+++ next-app-class-6/src/app/confirmation/page.tsx
@@ -1,6 +1,7 @@
+import Navbar from "../components/Navbar"
 export default function Confirmation(){
     return(
-        <div className="bg-slate-50 min-h-screen">
+        <div className="bg-slate-50 min-h-screen"><Navbar/>
             <div className="flex justify-center text-5xl font-bold italic text-blue-500 pt-32">Thank you for Shopping!!!</div>
             <div className="flex justify-center my-10">Your order will be delivered in 3 business days</div>
         </div>
