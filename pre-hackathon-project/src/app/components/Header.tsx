f40ad5adc66b5d31e92c649c73051388b01ea589
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2024-12-07 12:28:59 +0500

    Hero Section Completed

--- pre-hackathon-project/src/app/components/Header.tsx
+++ pre-hackathon-project/src/app/components/Header.tsx
@@ -1,16 +1,81 @@
 import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <div className="flex justify-end pr-20 text-white items-center bg-black py-4">
-      <div className="w-[859px] h-[24px] flex gap-6">
-        <p>
-          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-        </p>
-        <span className="font-semibold"><u>ShopNow</u></span>
+    <div>
+      <div className="flex justify-end pr-20 text-white items-center bg-black py-4">
+        <div className="w-[859px] h-[24px] flex gap-6">
+          <p>
+            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+          </p>
+          <span className="font-semibold">
+            <u>ShopNow</u>
+          </span>
+        </div>
+        <div className="flex gap-2">
+          <span>English</span>
+          <Image
+            src={"/dropdown.png"}
+            alt={"dopedown"}
+            width={24}
+            height={24}
+          />
+        </div>
       </div>
-      <div className="flex gap-2">
-        <span>English</span>
-        <Image src={"/dropdown.png"} alt={"dopedown"} width={24} height={24} />
+      <div className="flex justify-around mt-12 items-center">
+        <div>
+          <h2 className="text-xl font-bold">Exclusive</h2>
+        </div>
+        <div>
+          <ul className="flex gap-10">
+            <li className="border-b-2 hover:border-b-2">
+              <Link href={"#"}>Home</Link>
+            </li>
+            <li className="hover:border-b-2">
+              <Link href={"#"}>Contact</Link>
+            </li>
+            <li className="hover:border-b-2">
+              <Link href={"#"}>About</Link>
+            </li>
+            <li className="hover:border-b-2">
+              <Link href={"#"}>Signup</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex gap-8 items-center">
+          <div className="bg-slate-100 rounded flex justify-around gap items-center w-[243px] h-[38px]">
+            <div className="">
+              <input
+                type="search"
+                placeholder="What are you looking for?"
+                className="bg-slate-100 font-thin text-[12px] border-none p-1 w-[180px]"
+              />
+            </div>
+            <div className="w-4 h-4">
+              <Image
+                className="bg-slate-100"
+                src={"/search.svg"}
+                alt="search icon"
+                width={16}
+                height={16}
+              />
+            </div>
+          </div>
+          <div className="flex gap-5 w-6 h-6">
+          <Image
+              src={"/wishlist.svg"}
+              alt="wishlist"
+              width={16}
+              height={16}
+            /> <Image
+              src={"/cart1.svg"}
+              alt="wishlist"
+              width={18}
+              height={16}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- pre-hackathon-project/src/app/page.tsx
+++ pre-hackathon-project/src/app/page.tsx
@@ -3,7 +3,45 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div>
-       </div>
+    <div className="Main">
+      <div className="border-t-[1px] border-t-slate-200 my-4 w-full"></div>
+
+      <div className="Hero mt-10 flex justify-around">
+
+        <div className="Hero1">
+          <ul className="flex flex-col justify-between h-[344px]">
+            <li>Woman's Fashion</li>
+            <li>Man's Fashion</li>
+            <li>Electronics</li>
+            <li>Home & Electronics</li>
+            <li>Medicine</li>
+            <li>Sports & Outdoor</li>
+            <li>Baby & Toys</li>
+            <li>Grocery & Pets</li>
+            <li>Heath & Beauty</li>
+          </ul>
+        </div>
+
+        <div className="Hero2 w-[894px] h-[344px] flex flex-col items-center justify-center gap-10 bg-black overflow-hidden">
+          <div className="hero1 flex gap-44">
+            <div className="flex flex-col gap-8 pt-10">
+              <div><Image src={"/iphone-frame.svg"} alt="iphoneImg" width={190} height={49} /></div>
+              <div className="text-white text-5xl">Up to 10% <br /> off Voucher</div>
+
+              <div className="flex gap-2">
+                <div className="text-white text-[18px] font-semibold">Shop Now</div>
+                <div><Image src={"/icons arrow-right.svg"} alt="iphoneImg" width={24} height={24} /></div>
+              </div>
+
+            </div>
+            <div className="HeroiphoneImage relative top-10 left-14">
+              <Image src={"/HeroIphone.svg"} alt="iphoneHeroImg" width={400} height={24} />
+            </div>
+          </div>
+          <div className="dots"><Image src={"/dotFrame.svg"} alt="iphoneImg" width={100} height={24} /></div>
+        </div>
+      </div>
+
+    </div>
   );
 }
