4ee64cc045fc42fbc5cd765c2f33b1fd86e0f06f
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2024-12-09 13:03:44 +0500

    Update Service.tsx

--- hackathon-project/src/app/components/Service.tsx
+++ hackathon-project/src/app/components/Service.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Service(){
     return(
-        <div className="bg-[#F9F1E7] w-full h-60 mt-16 relative">
+        <div className="bg-[#F9F1E7] w-full h-52 mt-16 relative">
         <Image src={'/Feature.svg'}
         alt=""
         layout="fill"
