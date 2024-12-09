c44e54ebdd240f72554a114e2b67de850d9020b5
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2024-12-09 13:07:51 +0500

    blog page update

--- hackathon-project/src/app/blog/page.tsx
+++ hackathon-project/src/app/blog/page.tsx
@@ -76,7 +76,7 @@ export default function Blog() {
                     </div>
                     <h3 className="text-3xl">Handmade pieces that took time to make</h3>
                     <p className="text-[#9F9F9F] py-4 text-justify lg:w-[600px]">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum....
                     </p>
                     <button className="border-b-2 border-slate-400">Read More</button>
                 </div>
